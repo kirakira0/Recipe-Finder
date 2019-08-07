2601063e7b141f2f943245f4caeca55c6d2f4970
kirakira0 <kiratoal0@gmail.com>
2019-08-07 16:08:32 -0700

    Added a function that allows user to search by ingredients + added some initial js

---
+++ js/recipe.js
@@ -0,0 +1,8 @@
+const main = document.querySelector("#main"); 
+
+window.addEventListener('load', e =>{
+ 
+    //message.style.transform = 'rotate(200deg)'; 
+    //message.style.transform = 'rotate(270deg)'; 
+    main.style.opacity = 1; 
+})
--- main.py
+++ main.py
@@ -4,6 +4,11 @@ import json #lets you parse json files into python directories
 import jinja2
 from urllib import urlencode 
 from google.appengine.api import urlfetch
+# from pprint import pprint
+import pprint
+
+import logging
+
 
 # This initializes the jinja2 Environment.
 # This will be the same in every app that uses the jinja2 templating library.
@@ -15,17 +20,34 @@ the_jinja_env = jinja2.Environment(
 # the handler section
 class SearchFormHandler(webapp2.RequestHandler):
     def get(self): 
+        print("GET")
+        logging.debug("some helpful debug info")
+
         form_template = the_jinja_env.get_template('templates/form.html')
         self.response.write(form_template.render())  # the response 
 
 class RecipePageHandler(webapp2.RequestHandler):
     def post(self):
+        print("POST")
+        logging.debug("some helpful debug info")
+
         
         query = self.request.get('query') #take the value and save it as a python variable 
+        ingredients = self.request.get('ingredients')
         base_url = "http://www.recipepuppy.com/api/?" 
-        params = {'q':query} #put the query into a parameter list 
-        response = urlfetch.fetch(base_url + urlencode(params)).content #fetch a response 
+
+        logging.debug("INGREDIENTS: " + ingredients)
+
+        params = {'q':query, 'i':ingredients} #put the query into a parameter list 
+
+        api_url = base_url + urlencode(params)
+        logging.debug("API URL: " + api_url)
+
+        response = urlfetch.fetch(api_url).content #fetch a response 
         results = json.loads(response)
+        # pprint(results)
+        # logging.debug(pprint.pformat(results))
+
 
         recipe_template = the_jinja_env.get_template('templates/recipe.html')
         self.response.write(recipe_template.render({ #save into a dictionary 
